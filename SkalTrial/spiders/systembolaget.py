1d40c5f45b7c1f08f56c694f13ef4c27649cd1e8
gowthamparamasivam <gowthamparamasivam66@gmail.com>
2020-04-30 00:13:37 +0530

    Second commit

--- SkalTrial/spiders/systembolaget.py
+++ SkalTrial/spiders/systembolaget.py
@@ -5,7 +5,18 @@ import scrapy
 class SystembolagetSpider(scrapy.Spider):
     name = 'systembolaget'
     allowed_domains = ['www.systembolaget.com']
-    start_urls = ['http://www.systembolaget.com/']
+    start_urls = ['https://www.systembolaget.se/sok-dryck/?searchquery=Whisky']
 
     def parse(self, response):
+        #todo .result-list>.elm-product-list-item-full>a
+        #response.
+        print (response)
+        r = response.css(".result-list>.elm-product-list-item-full").getall();
+        #print ("ouput")
+        #print (r)
+        yield{
+            'output': r
+
+
+        }
         pass
